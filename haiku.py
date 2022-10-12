b06710097a789f21f665a6329fc5b5e84c904d09
Awes2000 <114576279+Awes2000@users.noreply.github.com>
2022-10-12 12:49:49 +0200

    Update haiku.py
    rijmen op Sam

--- haiku.py
+++ haiku.py
@@ -6,6 +6,7 @@ print("Deze haiku is van Awes")
 print("Deze haiku is van Sam")
 print("Wat rijmt op banaan?")
 print("Ik spreek je aan")
+print("We lopen in een laan")
 #laat de code hieronder met rust
 Haiku1()
 Haiku2()
